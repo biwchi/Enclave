cab67fc83bb8ee03fafd4cf542808c91f4cdc503
hodame <hodame04@gmail.com>
2023-08-20 16:01:55 +0500

    add logical

--- frontend/src/components/UI/Accordion.tsx
+++ frontend/src/components/UI/Accordion.tsx
@@ -1,27 +1,41 @@
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import ChevronLightIcon from '@/assets/ChevronLightIcon';
 
 type AccordionProps = {
   title: string;
   items: string[];
+  isOpened?: boolean;
+  onOpen?: () => void;
 };
 
-export default function Accordion({ title, items }: AccordionProps) {
+export default function Accordion({ title, items, isOpened, onOpen }: AccordionProps) {
   const [opened, setOpened] = useState(false);
+
+  function open() {
+    isOpened ? setOpened((value) => (value = !value)) : null;
+    onOpen ? onOpen() : null;
+  }
+
   return (
     <div>
       <div
-        onClick={() => setOpened((value) => (value = !value))}
-        className="flex cursor-pointer select-none items-center justify-between">
-        <h1 className="text-gray-900 text-sm font-medium">{title}</h1>
+        className="flex cursor-pointer select-none items-center justify-between p-5"
+        onClick={open}>
+        <h1 className="text-sm font-medium text-gray-900">{title}</h1>
         <ChevronLightIcon className="transition" rotate={opened ? '180deg' : '0deg'} />
       </div>
 
       <div
-        className={'mt-2 transition ' + (opened ? 'opacity-1 visible' : 'invisible h-0 opacity-0')}>
-        {items.map((item) => {
+        className={
+          ' transition ' + (isOpened ? 'opacity-1 visible mt-2' : 'invisible h-0 opacity-0')
+        }>
+        {items.map((item, idx) => {
           return (
-            <div className="text-gray-500 cursor-pointer py-2 text-sm hover:underline">{item}</div>
+            <div
+              key={idx}
+              className="cursor-pointer px-5 py-2 text-sm text-gray-500 hover:bg-gray-100">
+              {item}
+            </div>
           );
         })}
       </div>
