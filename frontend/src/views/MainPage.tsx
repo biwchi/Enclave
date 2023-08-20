9d7a1fc833be00858269fda95967d5775bfcfede
hodame <hodame04@gmail.com>
2023-08-20 16:02:04 +0500

    add main slider

--- frontend/src/views/MainPage.tsx
+++ frontend/src/views/MainPage.tsx
@@ -1,9 +1,17 @@
-import SearchBar from "@/components/SearchBar";
+import MainSlider from '@/components/MainSlider';
+import SearchBar from '@/components/SearchBar';
 
 export default function Main() {
   return (
     <>
       <SearchBar />
+      <MainSlider
+        slides={[
+          <div className="justify-center flex h-72 w-screen items-center bg-gray-200">1</div>,
+          <div className="justify-center flex h-72 w-screen items-center bg-gray-200">1</div>,
+          <div className="justify-center flex h-72 w-screen items-center bg-gray-200">1</div>
+        ]}
+      />
     </>
   );
 }
